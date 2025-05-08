3fe96a77444eb0199154f7db4a01b53b0dcd11ee
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2025-05-08 23:39:30 +0530

    api fix

--- src/components/News.js
+++ src/components/News.js
@@ -1,93 +1,73 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Newsitems from "./Newsitems";
 
-export class News extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalArt: 0,
-    };
-  }
+const News = () => {
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalArt, setTotalArt] = useState(0);
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=75db1db2637048cfbfb6f830a1383ee0&pageSize=5`;
-    let data = await fetch(url);
-    let parsData = await data.json();
-    this.setState({
-      articles: parsData.articles,
-      totalArt: parsData.totalResults,
-    });
-  }
-  handlePrevPage = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=75db1db2637048cfbfb6f830a1383ee0&page=${
-      this.state.page - 1
-    }&pageSize=5`;
-    let data = await fetch(url);
-    let parsData = await data.json();
-    this.setState({
-      articles: parsData.articles,
-      page: this.state.page - 1,
-    });
-  };
-  handleNextPage = async () => {
-    // console.log("right");
+  const pageSize = 5;
+  const apiKey = "75db1db2637048cfbfb6f830a1383ee0";
 
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=75db1db2637048cfbfb6f830a1383ee0&page=${
-      this.state.page + 1
-    }&pageSize=5`;
-    let data = await fetch(url);
-    let parsData = await data.json();
-    this.setState({
-      articles: parsData.articles,
-      page: this.state.page + 1,
-    });
+  const fetchNews = async (pageNumber) => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}&pageSize=${pageSize}&page=${pageNumber}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setArticles(data.articles);
+    setTotalArt(data.totalResults);
   };
 
-  render() {
-    return (
-      <>
-     
-        <div className="d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-primary btn-sm"
-            onClick={this.handlePrevPage}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={this.state.page >= 4}
-            type="button"
-            className="btn btn-primary btn-sm"
-            onClick={this.handleNextPage}
-          >
-            Next &rarr;
-          </button>
+  useEffect(() => {
+    fetchNews(page);
+  }, [page]);
+
+  const handlePrevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  const handleNextPage = () => {
+    if (page < Math.ceil(totalArt / pageSize)) {
+      setPage(page + 1);
+    }
+  };
+
+  return (
+    <>
+      <div className="d-flex justify-content-between my-3">
+        <button
+          disabled={page <= 1}
+          type="button"
+          className="btn btn-primary btn-sm"
+          onClick={handlePrevPage}
+        >
+          &larr; Previous
+        </button>
+        <button
+          disabled={page >= Math.ceil(totalArt / pageSize)}
+          type="button"
+          className="btn btn-primary btn-sm"
+          onClick={handleNextPage}
+        >
+          Next &rarr;
+        </button>
+      </div>
+      <hr />
+      <div className="container my-4">
+        <div className="row my-3">
+          {articles.map((element) => (
+            <div className="col-md-4" key={element.url}>
+              <Newsitems
+                title={element.title}
+                description={element.description}
+                imgUrl={element.urlToImage}
+                newsUrl={element.url}
+              />
+            </div>
+          ))}
         </div>
-        <hr/>
-        <div className="container my-4">
-          <div className="row my-3">
-            {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4 " key={element.url}>
-                  <Newsitems
-                    title={element.title}
-                    description={element.description}
-                    imgUrl={element.urlToImage}
-                    newsUrl={element.url}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default News;
